1f0e1436abc03becaf9565f187c7929f7fb1dbb5
Alvaro <sggp2586@gmail.com>
2020-10-12 21:51:33 -0700

    Initial Commit

---
+++ AppRedSocial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppRedSocial
+//
+//  Created by admin on 12/10/20.
+//  Copyright © 2020 Alvaro Fiestas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
