ce07d60e725cab02def7e68b99df7bd5e9429918
Alvaro <sggp2586@gmail.com>
2020-10-12 22:22:25 -0700

    #1

--- AppRedSocial/ViewController.swift
+++ AppRedSocial/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    
+   
+    @IBOutlet weak var pass: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func iniciar(_ sender: Any) {
+        // RIGHT CREDENTIALS...
+        let alertController = UIAlertController(title: "App", message: "Credenciales correctas", preferredStyle: .alert)
+        let action1 = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+        }
+        alertController.addAction(action1)
+         
+         
+         // WRONG EMAIL AND PASSWORD...
+         
+         let alertController2 = UIAlertController(title: "App", message: "Credenciales erróneas", preferredStyle: .alert)
+               let action2 = UIAlertAction(title: "OK", style: .destructive) { (action:UIAlertAction) in
+               }
+               alertController2.addAction(action2)
+        
+         
+         let email = "admin@gmail.com"
+         let password = "admin"
+         if self.txtEmail.text == email && self.pass.text == password
+         {
+         self.present(alertController, animated: true, completion: nil)
+         }
+         else
+         {
+             self.present(alertController2, animated: true, completion: nil)
+         }
+    }
+    
 }
 
