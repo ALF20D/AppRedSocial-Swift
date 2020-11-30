53fce5b4621605c2a9a996cb1f23e61050d67417
Alvaro Fiestas <sggp2586@gmail.com>
2020-11-29 16:44:33 -0800

    Proyecto

--- AppRedSocial/ViewsControllers/CommentsViewController.swift
+++ AppRedSocial/ViewsControllers/CommentsViewController.swift
@@ -55,7 +55,7 @@ class CommentsViewController: UIViewController, UITableViewDelegate, UITableView
        }
         else
        {
-        postUnitComment ()
+        postUnitComment()
        }
     }
     
