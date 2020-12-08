9dff0d9b401db0a3df2b03ed52630aaaa79a63c8
Alvaro Fiestas <sggp2586@gmail.com>
2020-12-08 14:24:15 -0800

    Profile

--- AppRedSocial/ViewsControllers/ProfileViewController.swift
+++ AppRedSocial/ViewsControllers/ProfileViewController.swift
@@ -10,34 +10,59 @@ import UIKit
 import Firebase
 
 class ProfileViewController: UIViewController {
-    
-    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nombreTextField: UITextField!
+    @IBOutlet weak var apellidoTextField: UITextField!
+    @IBOutlet weak var EditNombreButton: UIButton!
+    @IBOutlet weak var EditApellidoButton: UIButton!
+    @IBOutlet weak var GuardarButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupElements()
         Auth.auth().addStateDidChangeListener { (auth, user) in
-            
             if(user != nil)
-                
                    {
-                    
                    Firestore.firestore().collection("users")
                     .whereField("uid", isEqualTo: auth.currentUser?.uid as Any)
                      .getDocuments { (querySnapshot, err) in
                             if err != nil {
                                 print("Ocurrió un error.")
                             }
-                            //print(querySnapshot?.documents[0].data())
                             for document in querySnapshot!.documents {
-                                self.nameLabel.text = document.data()["nombre"] as? String
-                                print("Desde Profile: ",document.data()["nombre"] as? String)
+                                self.nombreTextField.text = document.data()["nombre"] as? String
+                                self.apellidoTextField.text = document.data()["apellido"] as? String
                             }
                        }
-                           
-                   }
-        }
+                }
+         }
 
+    }
+    
+    
+    @IBAction func NombreClick(_ sender: Any) {
+        GuardarButton.alpha = 1
+        nombreTextField.isUserInteractionEnabled = true
+        nombreTextField.becomeFirstResponder()
+    }
+    
+    
+    @IBAction func ApellidoClick(_ sender: Any) {
+        GuardarButton.alpha = 1
+        apellidoTextField.isUserInteractionEnabled = true
+        apellidoTextField.becomeFirstResponder()
+    }
+    
+    
+    @IBAction func GuardarClick(_ sender: Any) {
+        GuardarButton.alpha = 0
+        nombreTextField.isUserInteractionEnabled = false
+        apellidoTextField.isUserInteractionEnabled = false
+    }
+    
+    func setupElements()
+    {
+        GuardarButton.alpha = 0
+        Utilities.styleHollowButton(GuardarButton)
     }
     
 }
